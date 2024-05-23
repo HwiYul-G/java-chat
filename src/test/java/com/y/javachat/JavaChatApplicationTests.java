7492d0a26c7688df94483f49fce22897e487a169
kim <hwiyul_g@daum.net>
2024-05-23 21:23:53 +0900

    init

---
+++ src/test/java/com/y/javachat/JavaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.y.javachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
