2d442ecf9bdf24d1ac82472ba1206fdcf9062636
kim <hwiyul_g@daum.net>
2024-05-29 16:41:32 +0900

    feat: ObjectNotFoundException

---
+++ src/main/java/com/y/javachat/system/exception/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package com.y.javachat.system.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String objectName, Long id) {
+        super("id: "+ id + "를 가진" + objectName + "을 찾을 수 없습니다.");
+    }
+
+    public ObjectNotFoundException(String objectName, String id) {
+        super(id + "를 가진" + objectName + "을 찾을 수 없습니다.");
+    }
+
+}
