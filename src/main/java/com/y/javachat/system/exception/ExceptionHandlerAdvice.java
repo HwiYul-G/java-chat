8be6d0885a0cc7c6108cad56996348eb9eb678aa
kim <hwiyul_g@daum.net>
2024-05-29 20:59:31 +0900

    feat: ExceptionHandlerAdvice - NotFoundException이 발생했을 때 그것을 받아서 Result로 변환시켜넘김

---
+++ src/main/java/com/y/javachat/system/exception/ExceptionHandlerAdvice.java
@@ -0,0 +1,39 @@
+package com.y.javachat.system.exception;
+
+import com.y.javachat.system.Result;
+import com.y.javachat.system.StatusCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ExceptionHandlerAdvice {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    Result handlerUserNotFoundException(ObjectNotFoundException e) {
+        return new Result(false, StatusCode.NOT_FOUND, e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    Result handleValidationException(MethodArgumentNotValidException e) {
+        List<ObjectError> errors = e.getBindingResult().getAllErrors();
+        Map<String, String> map = new HashMap<>(errors.size());
+        errors.forEach((error) -> {
+            String key = ((FieldError) error).getField();
+            String val = error.getDefaultMessage();
+            map.put(key, val);
+        });
+        return new Result(false, StatusCode.INVALID_ARGUMENT, "제공된 인자가 타당하지 않습니다.", map);
+    }
+
+}
