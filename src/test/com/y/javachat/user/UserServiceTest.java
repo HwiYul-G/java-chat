33bddcf8d065fe211ba3f60bb75a0413898803c4
kim <hwiyul_g@daum.net>
2024-05-29 20:59:48 +0900

    style: 불필요한 임포트 제거

--- src/test/com/y/javachat/user/UserServiceTest.java
+++ src/test/com/y/javachat/user/UserServiceTest.java
@@ -9,7 +9,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
