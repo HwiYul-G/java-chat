f3fcb80ca407cbe5c35022d2b43bd7f11755b27c
kim <hwiyul_g@daum.net>
2024-05-30 15:31:50 +0900

    feat(SecurityConfig): filterChain에 user 관련 CRUD에 권한 등록 및 h2 db는 전부 접근 가능하게 설정 + passwordEncoder

---
+++ src/main/java/com/y/javachat/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.y.javachat.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${api.endpoint.base-url}")
+    private String baseUrl;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                                .requestMatchers(HttpMethod.GET, baseUrl + "/users/**").hasAuthority("admin") // Protect the endpoint
+                                .requestMatchers(HttpMethod.POST, baseUrl + "/users").hasAuthority("admin")
+                                .requestMatchers(HttpMethod.PUT, baseUrl + "/users/**").hasAuthority("admin")
+                                .requestMatchers(HttpMethod.DELETE, baseUrl + "/users/**").hasAuthority("admin")
+                                .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
+                                // Disallow everything else.
+                                .anyRequest().authenticated() // 마지막엔 항상 이 조건을 둔다.
+                        // requestMatcher(HttpMethod.Get, baseUrl + "/users/**).permitAll()
+                )
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) // for H2 browser console access
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+}
