e97f91fe2114f6942bc714e56bf15053953f8bb7
kim <hwiyul_g@daum.net>
2024-06-07 20:26:16 +0900

    feat: Enable Async

--- src/main/java/com/y/javachat/JavaChatApplication.java
+++ src/main/java/com/y/javachat/JavaChatApplication.java
@@ -2,7 +2,9 @@ package com.y.javachat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class JavaChatApplication {
 
