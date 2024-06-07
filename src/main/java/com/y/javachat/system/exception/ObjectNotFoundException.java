ef991cd94c57642d3aa2e09f11a895ea97eb488a
kim <hwiyul_g@daum.net>
2024-06-07 21:26:21 +0900

    refactor(ObjectNotFoundException)

--- src/main/java/com/y/javachat/system/exception/ObjectNotFoundException.java
+++ src/main/java/com/y/javachat/system/exception/ObjectNotFoundException.java
@@ -6,8 +6,8 @@ public class ObjectNotFoundException extends RuntimeException {
         super("id: "+ id + "를 가진 " + objectName + "을 찾을 수 없습니다.");
     }
 
-    public ObjectNotFoundException(String objectName, String id) {
-        super("id: "+ id + "를 가진 " + objectName + "을 찾을 수 없습니다.");
+    public ObjectNotFoundException(String objectName, String info) {
+        super("info: "+ info + "를 가진 " + objectName + "을 찾을 수 없습니다.");
     }
 
 }
