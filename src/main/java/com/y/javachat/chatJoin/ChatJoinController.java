034b09f37f33ebddc9c45f533f88a5814043af5f
kim <hwiyul_g@daum.net>
2024-06-10 15:27:34 +0900

    feat(ChatJoinService, ChatJoinController)

---
+++ src/main/java/com/y/javachat/chatJoin/ChatJoinController.java
@@ -0,0 +1,25 @@
+package com.y.javachat.chatJoin;
+
+import com.y.javachat.system.Result;
+import com.y.javachat.system.StatusCode;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.endpoint.base-url}/chat-join")
+public class ChatJoinController {
+
+    private final ChatJoinService chatJoinService;
+
+    @PostMapping()
+    public Result addChatJoin(@Valid @RequestBody ChatJoin chatJoin){
+        ChatJoin savedChatJoin = chatJoinService.save(chatJoin);
+        return new Result(true, StatusCode.SUCCESS, "채팅 조인 성공", savedChatJoin);
+    }
+
+}
