00b7fa9b8626b9ded5d1b757030860fcc8e1194b
kim <hwiyul_g@daum.net>
2024-06-28 22:03:29 +0900

    [front]  비율 수정

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Sidebar from '../components/Sidebar'
 
 const Home = () => {
+  
   return (
-    <div>
-      Home
-      
+    <div className='container row'>
+      <section className='col-4'>
+        <Sidebar/>
+      </section>
       {/* message component */}
-      <section>
+      <section className='col-8'>
         <Outlet/>
       </section>
     </div>
