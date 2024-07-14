2abffa32d864114d85aa27639b6ad4048a30df2e
kim <hwiyul_g@daum.net>
2024-07-14 12:26:22 +0900

    [frontend] contextAPI로 자주 사용하는 사용자 정보를 등록

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
+import { UserProvider } from './context/UserContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <UserProvider>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </UserProvider>
   </React.StrictMode>
 );
