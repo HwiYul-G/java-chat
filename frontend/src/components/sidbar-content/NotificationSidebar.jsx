98323fe13832ef8ef057ba8b946e6d517dda503b
kim <hwiyul_g@daum.net>
2024-07-20 15:59:23 +0900

    [front] 친구 추가 알림 부분 추가

--- frontend/src/components/sidbar-content/NotificationSidebar.jsx
+++ frontend/src/components/sidbar-content/NotificationSidebar.jsx
@@ -1,7 +1,44 @@
+import './css/_common.css';
+import AddFriendNotification from './contents/AddFriendNotification';
+
 const NotificationSidebar = () => {
+    
+    const invitationInfos = [
+        {
+            invitationId: 1,
+            inviterName: "김철수",
+            invitationDate: "2024년 7월 20일 04:45 PM"
+        },
+        {
+            invitationId: 2,
+            inviterName: "이영희",
+            invitationDate: "2024년 7월 20일 05:15 PM"
+        },
+        {
+            invitationId: 3,
+            inviterName: "박민수",
+            invitationDate: "2024년 7월 20일 06:00 PM"
+        }
+    ];
+
     return (
-        <div>
-            알림
+        <div className='d-flex flex-column h-100'>
+            <div className='tab-header'>
+                <h5>알림</h5>
+            </div>
+
+            <div className='hide-scrollbar h-100'>
+                <div className='m-4 mt-0'>
+                    <div className='notification-list'>
+                        {
+                            invitationInfos.map(info => (
+                                <AddFriendNotification key={info.invitationId} {...info} />
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
---
+++ frontend/src/components/sidbar-content/contents/AddFriendNotification.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const AddFriendNotification = ({invitationId, inviterName, invitationDate}) => {
+    const [isResponded, setIsResponded] = useState(false);
+
+    const handleReject = () => {
+        setIsResponded(true);
+    };
+
+    const handleAccept = () => {
+        setIsResponded(true);
+    };
+
+    return (
+        <div className='card mb-3'>
+            <div className='card-body'>
+                <div className='d-flex align-items-center'>
+                    <div className='flex-grow-1'>
+                        <div className='d-flex align-items-center overflow-hidden'>
+                            <h5 className='me-auto text-break mb-0'>{inviterName}</h5>
+                            <span className='small text-muted text-nowrap ms-2'>{invitationDate}</span>
+                        </div>
+                        <div className='d-flex align-items-center'>
+                            <div className='line-clamp me-auto'>당신에게 친구 초대를 요청합니다.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {!isResponded && (
+                <div className='card-footer'>
+                    <div className='row gx-4'>
+                        <div className='col'>
+                            <button className='btn btn-secondary btn-sm w-100' onClick={handleReject}>거절</button>
+                        </div>
+                        <div className='col'>
+                            <button className='btn btn-primary btn-sm w-100' onClick={handleAccept}>수락</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default AddFriendNotification;
