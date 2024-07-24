3edbb74ae89c5369f3ee1e52257cffea66719ba0
kim <hwiyul_g@daum.net>
2024-07-25 00:12:50 +0900

    [backend] chat type 추가

--- backend/src/main/java/com/y/javachat/chat/Chat.java
+++ backend/src/main/java/com/y/javachat/chat/Chat.java
@@ -27,4 +27,6 @@ public class Chat {
     private String content;
 
     private Timestamp createdAt;
+
+    private MessageType type;
 }
---
+++ backend/src/main/java/com/y/javachat/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.y.javachat.chat;
+
+public enum MessageType {
+    CONNECT,
+    DISCONNECT,
+    CHAT,
+    DATE
+}
