3de6cf4ef0e25226d712d8eb4a3a8f19169c1d5d
kim <hwiyul_g@daum.net>
2024-07-26 18:04:44 +0900

    [backend] feat(ChatController): 특정 채팅방의 id로 해당 방의 채팅 메시지 내역을 받아오는 api

--- backend/src/main/java/com/y/javachat/chat/ChatController.java
+++ backend/src/main/java/com/y/javachat/chat/ChatController.java
@@ -1,13 +1,19 @@
 package com.y.javachat.chat;
 
+import com.y.javachat.system.Result;
+import com.y.javachat.system.StatusCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -21,6 +27,13 @@ public class ChatController {
     public Chat sendMessage(@Payload Chat chat) {
         chat.setCreatedAt(LocalDateTime.now());
         return chatService.save(chat);
+    }
+
+    @GetMapping("${api.endpoint.base-url}/chat-rooms/{roomId}/messages")
+    @ResponseBody
+    public Result getAllMessagesByRoomId(@PathVariable Long roomId){
+        List<Chat> chats = chatService.findAllByRoomId(roomId);
+        return new Result(true, StatusCode.SUCCESS, "채팅 리스트 조회 성공", chats);
     }
 
 }
