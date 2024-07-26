6c05618b85f8a6423a6e47ebbc610b8cb8616927
kim <hwiyul_g@daum.net>
2024-07-26 18:01:54 +0900

    [front] stomp 연결 및 pub sub 관련

---
+++ frontend/src/stomp/index.js
@@ -0,0 +1,66 @@
+import * as StompJs from '@stomp/stompjs';
+
+const client = new StompJs.Client({
+    brokerURL: 'ws://localhost:8080/java-chat',
+    connectHeaders: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+    },
+    debug: function (str){
+        console.log(str);
+    },
+    reconnectDelay: 5000,
+    heartbeatIncoming: 4000,
+    heartbeatOutgoing: 4000,
+});
+
+client.onConnect = function (frame) {
+    console.log("Connected!");
+    if (client.subscriptionRequests) {
+        client.subscriptionRequests.forEach(sub => {
+            client.subscribe(sub.destination, sub.callback);
+        });
+        client.subscriptionRequests = [];
+    }
+};
+
+client.onStompError = function(frame){
+    console.log('Broker reported error: ' + frame.headers['message']);
+    console.log('Additional details: ' + frame.body);
+};
+
+const activateClient = () => {
+    if(!client.active)
+        client.activate();
+};
+
+const deactivateClient = () => {
+    if(client.active)
+        client.deactivate();
+};
+
+const sendMessage = (roomId, message) => {
+    client.publish({
+        destination: `/pub/chat-rooms/${roomId}`,
+        body: JSON.stringify(message),
+        headers: {
+            'content-type': 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+    });
+};
+
+const subscribeToRoom = (roomId, callback) => {
+    const destination = `/sub/chat-rooms/${roomId}`;
+    if (client.connected) {
+        client.subscribe(destination, message => {
+            callback(JSON.parse(message.body));
+        });
+    } else {
+        if (!client.subscriptionRequests) {
+            client.subscriptionRequests = [];
+        }
+        client.subscriptionRequests.push({ destination, callback });
+    }
+};
+
+export {activateClient, deactivateClient, sendMessage, subscribeToRoom};
