72f543bc215306daaaa6ea20e4285eb4d2312ff2
kim <hwiyul_g@daum.net>
2024-07-29 00:42:09 +0900

    [backend] userId와 friendId로 friendship 찾기 dao 추가

--- backend/src/main/java/com/y/javachat/app/repository/FriendshipRepository.java
+++ backend/src/main/java/com/y/javachat/app/repository/FriendshipRepository.java
@@ -5,9 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FriendshipRepository extends JpaRepository<Friendship,Long> {
 
     List<Friendship> findAllByUserId(Long userId);
+
+    Optional<Friendship> findByUserIdAndAndFriendId(Long userId, Long friendId);
+
 }
