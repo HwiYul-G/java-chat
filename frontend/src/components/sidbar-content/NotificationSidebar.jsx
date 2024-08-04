a26554f6e786a13a493b95d73ff539d600ff1e34
kim <hwiyul_g@daum.net>
2024-08-04 14:22:18 +0900

    [front] 친구 요청 목록 api 적용

--- frontend/src/components/sidbar-content/NotificationSidebar.jsx
+++ frontend/src/components/sidbar-content/NotificationSidebar.jsx
@@ -1,25 +1,33 @@
 import './css/_common.css';
 import AddFriendNotification from './contents/AddFriendNotification';
+import { useEffect, useState } from 'react';
+import { useUser } from '../../context/UserContext';
+import { findFriendRequests } from '../../api/userApi';
+import moment from 'moment';
 
 const NotificationSidebar = () => {
-    
-    const invitationInfos = [
-        {
-            invitationId: 1,
-            inviterName: "김철수",
-            invitationDate: "2024년 7월 20일 04:45 PM"
-        },
-        {
-            invitationId: 2,
-            inviterName: "이영희",
-            invitationDate: "2024년 7월 20일 05:15 PM"
-        },
-        {
-            invitationId: 3,
-            inviterName: "박민수",
-            invitationDate: "2024년 7월 20일 06:00 PM"
-        }
-    ];
+    const { userInfo } = useUser();
+
+    const [invitationInfos, setInvitationInfos] = useState([]);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        const fetchFriendRequests = async () => {
+            try{
+                const data = await findFriendRequests(userInfo.id);
+                const invitationInfoData = data.data.map(info => ({
+                    invitationId: info.id,
+                    inviterName: info.senderName,
+                    invitationDate: moment(info.createdAt).format('')
+                }));
+                setInvitationInfos(invitationInfoData);
+            }catch(err){
+                setError("친구 요청 정보를 가져오는 데 실패했습니다.");
+            }
+        };
+
+        fetchFriendRequests();
+    }, []);
 
     return (
         <div className='d-flex flex-column h-100'>
@@ -29,6 +37,7 @@ const NotificationSidebar = () => {
 
             <div className='hide-scrollbar h-100'>
                 <div className='m-4 mt-0'>
+                    {error && <p className="error-message">{error}</p>}
                     <div className='notification-list'>
                         {
                             invitationInfos.map(info => (
