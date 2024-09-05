6c71321b834d0a7f7b51732fe61625bc45cfd25e
kim <hwiyul_g@daum.net>
2024-09-05 17:53:31 +0900

    [front] refactor: api 변경과 response 변경에 따른 그룹 채팅방 들어가기 수정

--- frontend/src/components/sidbar-content/contents/EnterGroupChatRoom.jsx
+++ frontend/src/components/sidbar-content/contents/EnterGroupChatRoom.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import { useUser } from "../../../context/UserContext";
-import { useNavigate } from "react-router-dom";
-import { enterGroupChatRoom } from "../../../api/groupChatJoinApi";
+import {useState} from "react";
+import {useUser} from "../../../context/UserContext";
+import {useNavigate} from "react-router-dom";
+import {enterGroupChatRoom} from "../../../api/chatApi";
 
 const EnterGroupChatRoom = () => {
     const {userInfo} = useUser();
@@ -10,19 +10,28 @@ const EnterGroupChatRoom = () => {
 
     const navigate = useNavigate();
 
-    const handleEnterClicked = async(e) => {
+    const handleEnterClicked = async (e) => {
         e.preventDefault();
         e.stopPropagation();
-        try{
-            const result = await enterGroupChatRoom({roomId: roomId, userId: userInfo.id});
-            if(result.flag){
-                navigate(`group/${roomId}`, 
-                    {state: {
-                        groupChatRoomName: result.data.name
-                    }}
-                );
+        try {
+            const result = await enterGroupChatRoom(roomId, userInfo.id);
+
+            if (result.flag) {
+                const {isGroup, chatRoomInfo} = result.data;
+
+                if (isGroup) {
+                    navigate(`chat-rooms/${result.data.roomId}`, {
+                        state: {
+                            groupChatRoomName: chatRoomInfo.roomName
+                        }
+                    });
+                } else {
+                    setMessage('그룹 채팅방이 아닙니다.');
+                }
+            } else {
+                setMessage('채팅방 입장에 실패했습니다.');
             }
-        }catch(err){
+        } catch (err) {
             setMessage(err.message);
         }
     };
@@ -31,9 +40,13 @@ const EnterGroupChatRoom = () => {
         <div className="card mb-3">
             <div className="card-body pt-0">
                 <form>
-                    <input type="text" placeholder="채팅방 번호를 입력해주세요."
-                    className="form-control form-control-sm form-control-solid"
-                    value={roomId} onChange={(e) => {setRoomId(e.target.value)}}/>
+                    <input
+                        type="text"
+                        placeholder="채팅방 번호를 입력해주세요."
+                        className="form-control form-control-sm form-control-solid"
+                        value={roomId}
+                        onChange={(e) => {setRoomId(e.target.value)}}
+                    />
                     <button className="btn btn-primary" onClick={handleEnterClicked}>
                         참여
                     </button>
