3b382ee0c3e739eb31ff5fa709d29c721631d32d
kim <hwiyul_g@daum.net>
2024-09-05 18:18:24 +0900

    [front] refactor: 개인채팅방과 그룹 채팅방 통합

--- frontend/src/components/ChatMessage.jsx
+++ frontend/src/components/ChatMessage.jsx
@@ -1,21 +1,22 @@
 import moment from "moment";
 
-const GroupMessage = ({message, userId}) => {
-    const {id, senderId, roomId, senderName, content, createdAt, type } = message;
+const ChatMessage = ({message, userId}) => {
+    const {id, roomId, senderId, senderName, content, createdAt, type} = message;
 
-    if(type === 'DATE'){
+    if (type === 'DATE') {
         return (
             <div class="divider d-flex align-items-center mb-4">
-              <p class="text-center mx-3 mb-0" style={{color: '#a2aab7'}}>{moment(createdAt).format('')}</p>
+                <p class="text-center mx-3 mb-0" style={{color: '#a2aab7'}}>{moment(createdAt).format('')}</p>
             </div>
         );
     }
 
-    if(userId === senderId){
+    if (userId === senderId) {
         return (
             <div className='d-flex flex-row justify-content-end mb-4'>
                 <div>
-                    <p className='small p-2 me-3 mb-1 text-white rounded-3 bg-primary' style={{maxWidth:'500px', wordWrap:'break-word'}}>{content}</p>
+                    <p className='small p-2 me-3 mb-1 text-white rounded-3 bg-primary'
+                       style={{maxWidth: '500px', wordWrap: 'break-word'}}>{content}</p>
                     <p className='small me-3 mb-3 rounded-3 text-muted d-flex justify-content-end'>{moment(createdAt).format('hh:mm')}</p>
                 </div>
             </div>
@@ -25,11 +26,12 @@ const GroupMessage = ({message, userId}) => {
         <div className='d-flex flex-row justify-content-start'>
             <p className="small mb-0 me-3">{senderName}</p>
             <div>
-                <p className="small p-2 mb-1 rounded-3 bg-body-tertiary" style={{maxWidth:'500px', wordWrap:'break-word'}}>{content}</p>
+                <p className="small p-2 mb-1 rounded-3 bg-body-tertiary"
+                   style={{maxWidth: '500px', wordWrap: 'break-word'}}>{content}</p>
                 <p className="small mb-3 rounded-3 text-muted">{moment(createdAt).format('hh:mm')}</p>
             </div>
         </div>
     );
 };
 
-export default GroupMessage;
+export default ChatMessage;
