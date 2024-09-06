ff540749aa121db89b5751930b43e99de526171e
kim <hwiyul_g@daum.net>
2024-09-06 14:12:41 +0900

    [front] fix: requestparam 넘기는 것 수정

--- frontend/src/api/notificationApi.js
+++ frontend/src/api/notificationApi.js
@@ -2,24 +2,24 @@ import instance from ".";
 
 const prefix = "/notifications";
 
-export const inviteFriend = async(userId, friendEmail) => {
-    try{
-        const requestData =  {"userId" : userId, "friendEmail" : friendEmail}
-        const res = instance.post(`${prefix}/friend-invitation`, requestData);
+export const inviteFriend = async (userId, friendEmail) => {
+    try {
+        const requestData = {"userId": parseInt(userId), "friendEmail": friendEmail}
+        const res = await instance.post(`${prefix}/friend-invitation`, requestData);
         console.log(res.data);
         return res.data;
-    } catch (err){
+    } catch (err) {
         console.error(err);
         throw err;
     }
 };
 
 export const updateNotificationStatus = async (notificationId, isAccept) => {
-    try{
-        const res = await instance.put(`${prefix}/${notificationId}`, isAccept);
+    try {
+        const res = await instance.put(`${prefix}/${notificationId}?isAccept=${isAccept}`);
         console.log(res.data);
         return res.data;
-    } catch(err){
+    } catch (err) {
         console.error(err);
         throw err;
     }
--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -3,79 +3,84 @@ import instance from ".";
 const prefix = "/users";
 
 export const register = async (newUser) => {
-    try{
+    try {
         // const newUser = {"email": , "username": , "password": , "roles": "user"}
         const res = await instance.post(`${prefix}`, newUser);
         console.log(res.data);
         return res.data;
-    }catch(err){
+    } catch (err) {
         console.error("서버의 회원가입 API 호출 실패: ", err);
         throw err;
     }
 };
 
 export const updateUserInfo = async (userId, userInfo) => {
-    try{
+    try {
         // {email: "", username:"" , roles: "user"}
         const res = await instance.put(`${prefix}/${userId}`, userInfo);
         console.log(res.data);
         return res.data;
-    } catch(err){
+    } catch (err) {
         console.error("서버의 사용자 정보 업데이트 API 호출 실패: ", err);
         throw err;
     }
 };
 
 export const deleteAccount = async (userId) => {
-    try{
+    try {
         const res = await instance.delete(`${prefix}/${userId}`);
         console.log(res.data);
         return res.data;
-    } catch(err){
+    } catch (err) {
         console.error("서버의 탈퇴하기 API 호출 실패: ", err);
         throw err;
     }
 };
 
 export const getFriends = async (userId) => {
-    try{
+    try {
         const res = await instance.get(`${prefix}/${userId}/friends`);
         console.log(res.data);
         return res.data;
-    }catch(err){
+    } catch (err) {
         console.error("서버의 친구 목록 조회 API 호출 실패: ", err);
         throw err;
     }
 };
 
 export const getNotifications = async (userId) => {
-    try{
+    try {
         const res = await instance.get(`${prefix}/${userId}/notifications`);
         console.log(res.data);
         return res.data;
-    }catch (err){
+    } catch (err) {
         console.error("서버의 알림 목록 조회 API 호출 실패: ", err);
         throw err;
     }
 };
 
-const getChatRooms = async(userId) => {
-    try{
-        const res = await instance.get(`${prefix}/${userId}/chat-rooms`);
+const getChatRooms = async (userId, isGroup) => {
+    try {
+        const res = await instance.get(`${prefix}/${userId}/chat-rooms`,
+            {
+                params: {
+                    isGroup: isGroup,
+                }
+            });
         console.log(res.data);
         return res.data;
-    } catch(err){
+    } catch (err) {
         console.error("서버의 채팅방 목록 조회 API 호출 실패: ", err);
         throw err;
     }
 };
 
 export const getPersonalChatRooms = async (userId) => {
-    const chatRooms = await getChatRooms(userId);
-    return chatRooms.data.filter(room => !room.isGroup);
+    const chatRooms = await getChatRooms(userId, false);
+    return chatRooms.data;
 };
 
 export const getGroupChatRooms = async (userId) => {
-    const chatRooms = await getChatRooms(userId);
-    return chatRooms.data.filter(room => room.isGroup);
+    const chatRooms = await getChatRooms(userId, true);
+    return chatRooms.data;
 };
