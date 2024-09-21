aa6d6dae16123051af7bf349ea787290dc5bdbfa
kim <hwiyul_g@daum.net>
2024-09-22 00:03:38 +0900

    [AI server] add bible

---
+++ ai/ai-server/src/main/java/com/y/aiserver/bible/ExceptionHandlerAdvice.java
@@ -0,0 +1,37 @@
+package com.y.aiserver.bible;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ExceptionHandlerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    Result handleValidationException(MethodArgumentNotValidException e) {
+        List<ObjectError> errors = e.getBindingResult().getAllErrors();
+        Map<String, String> map = new HashMap<>(errors.size());
+        errors.forEach((error) -> {
+            String key = ((FieldError) error).getField();
+            String val = error.getDefaultMessage();
+            map.put(key, val);
+        });
+        return new Result(false, StatusCode.INVALID_ARGUMENT, "제공된 인자가 타당하지 않습니다.", map);
+    }
+
+    // fallback(대비책): 예상 밖의 오류를 받아 처리
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    Result handleOtherException(Exception e) {
+        return new Result(false, StatusCode.INTERNAL_SERVER_ERROR, "서버 내부 오류", e.getMessage());
+    }
+}
---
+++ ai/ai-server/src/main/java/com/y/aiserver/bible/Result.java
@@ -0,0 +1,29 @@
+package com.y.aiserver.bible;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Result {
+    private boolean flag;
+    private Integer code;
+    private String message;
+    private Object data;
+
+    public Result(boolean flag, Integer code, String message) {
+        this.flag = flag;
+        this.code = code;
+        this.message = message;
+    }
+
+    public Result(boolean flag, Integer code, String message, Object data) {
+        this.flag = flag;
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+}
